c00d4d05bb7a52ed94b380ebdd65674cd8a74410
qianfeng0 <qianfeng0@hasaji.local>
2016-04-01 14:50:40 +0800

    第一次创建项目

---
+++ aaaGitDemo/aaaGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaGitDemo
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 郑志恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaGitDemo/aaaGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaGitDemo
+//
+//  Created by qianfeng0 on 16/4/1.
+//  Copyright © 2016年 郑志恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
